646903c126a677d75f5e32569b49511ee131706d
Lydia.Gardner <A02291141@usu.edu>
2024-05-23 01:21:58 -0600

    Implement a simple API for adding users to channels (not using the database yet)

--- backend/internal/server/apis/chat/chat.go
+++ backend/internal/server/apis/chat/chat.go
@@ -1,6 +1,8 @@
 package chat
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/jak103/powerplay/internal/server/apis"
 	"github.com/jak103/powerplay/internal/server/services/auth"
@@ -8,11 +10,13 @@ import (
 	"github.com/jak103/powerplay/internal/utils/responder"
 )
 
-var channels []ChannelConfiguration
+var nextID int = 0
+var channels = make(map[string]ChannelConfiguration)
 
 func init() {
 	apis.RegisterHandler(fiber.MethodGet, "/hello", auth.Public, helloWorld)
 	apis.RegisterHandler(fiber.MethodPost, "/chat/channels/create", auth.Public, createChannel)
+	apis.RegisterHandler(fiber.MethodPut, "/chat/channels/adduser", auth.Public, addUser)
 }
 
 func helloWorld(c *fiber.Ctx) error {
@@ -30,10 +34,10 @@ func createChannel(c *fiber.Ctx) error {
 	// Verify that values were provided for required fields. If any required values are missing, the request is bad.
 	var errorMsg string
 	if channel.Name == "" {
-		errorMsg = errorMsg + "\t'name' is a required field.\n"
+		errorMsg += "\t'name' is a required field.\n"
 	}
 	if channel.MemberIDs == nil {
-		errorMsg = errorMsg + "\t'member_ids' is a required field.\n"
+		errorMsg += "\t'member_ids' is a required field.\n"
 	}
 	if errorMsg != "" {
 		log.Info("Channel creation failed. Reason(s):\n" + errorMsg)
@@ -41,9 +45,51 @@ func createChannel(c *fiber.Ctx) error {
 	}
 
 	// Create a channel using the provided data
-	channels = append(channels, *channel) // TODO: store channels in the DB instead of just in a slice.
+	channels[strconv.Itoa(nextID)] = *channel // TODO: store channels in the DB instead of just in a dictionary.
+	nextID += 1
 	log.Info("Channel created: " + channel.Name)
 	return responder.Ok(c)
+}
+
+func addUser(c *fiber.Ctx) error {
+	updateData := new(ChannelUpdate)
+
+	// Load the request body as a ChannelUpdate object. If any of the provided values are the wrong type, the request is bad.
+	if err := c.BodyParser(updateData); err != nil {
+		return responder.BadRequest(c)
+	}
+
+	// Verify that values were provided for required fields and verify the existence of the channel. If any required values are missing or the channel doesn't exist, the request is bad.
+	var errorMsg string
+	var channel ChannelConfiguration
+	var channelOk bool
+	if updateData.ChannelID == "" {
+		errorMsg += "\t'channel_id' is a required field.\n"
+	} else {
+		// Retrieve the channel specified in the channel_id field.
+		channel, channelOk = channels[updateData.ChannelID] // TODO: retrieve the channel from the DB.
+		if !channelOk {
+			errorMsg += "\tNo channel exists with ID " + updateData.ChannelID + ".\n"
+		}
+	}
+	if updateData.Value == "" {
+		errorMsg += "\t'value' is a required field.\n"
+	}
+	// TODO: verify that the provided value is a valid user ID
+	// TODO: verify that the user ID has not already been added to the channel
+	if errorMsg != "" {
+		log.Info("The user could not be added to the channel. Reason(s):\n" + errorMsg)
+		return responder.BadRequest(c)
+	}
+
+	channel.MemberIDs = append(channel.MemberIDs, updateData.Value) // TODO: update the member_ids list in the database
+	log.Info("User " + updateData.Value + " added to channel " + updateData.ChannelID)
+	return responder.Ok(c)
+}
+
+type ChannelUpdate struct {
+	ChannelID string `json:"channel_id"`
+	Value     string `json:"value"`
 }
 
 type ChannelConfiguration struct {
