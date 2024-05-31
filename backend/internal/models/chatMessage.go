850596560d595f8df7c0f88d3e1cfba356964251
Septimis <meads.connoredward@gmail.com>
2024-05-31 11:21:34 -0600

    messages can be send between clients, however, browser refresh is needed each time

--- backend/internal/models/chatMessage.go
+++ backend/internal/models/chatMessage.go
@@ -4,14 +4,14 @@ import "time"
 
 type ChatMessage struct {
 	DbModel
-	MessageID		uint		'json:"id" db: "id"' // could be a string for UUID
-	SenderID		uint		'json:"senderID" db:"senderID"'
-	Content			string		'json:"content" db:"content"'
-	CreatedAt		time.Time	'json:"createdAt" db:"createdAt"'
-	LastModifiedAt	time.Time	'json:"lastModifiedAt" db:"lastModifiedAt'
-	DeletedAt		time.Time	'json:"deletedAt" db:"deletedAt"'
-	RecipientID		uint		'json:"recipientID" db:"recipientID"'
-	AttachmentURLs	[]string	'json:"attachmentURLs" db:"attachmentURLs"'
-	Reactions		[]string	'json:"reactions" db:"reactions"' // assuming emojis are strings
-	ParentID		uint		'json:"parentID" db:"parentID"'
+	MessageID      string    `json:"messageID" db:"messageID"` // could be a string for UUID
+	SenderID       string    `json:"senderID" db:"senderID"`
+	Content        string    `json:"content" db:"content"`
+	CreatedAt      time.Time `json:"createdAt" db:"createdAt"`
+	LastModifiedAt time.Time `json:"lastModifiedAt" db:"lastModifiedAt"`
+	DeletedAt      time.Time `json:"deletedAt" db:"deletedAt"`
+	RecipientID    string    `json:"recipientID" db:"recipientID"`
+	AttachmentURLs []string  `json:"attachmentURLs" db:"attachmentURLs"`
+	Reactions      []string  `json:"reactions" db:"reactions"` // assuming emojis are strings
+	ParentID       string    `json:"parentID" db:"parentID"`
 }
--- backend/internal/server/apis/chat/chat.go
+++ backend/internal/server/apis/chat/chat.go
@@ -2,62 +2,45 @@ package chat
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/jak103/powerplay/internal/models"
 	"github.com/jak103/powerplay/internal/server/apis"
 	"github.com/jak103/powerplay/internal/server/services/auth"
 )
 
-// Most simple message structure
-type Message struct {
-	From    string `json:"from"`
-	To      string `json:"to"`
-	Content string `json:"content"`
-}
-
 // Pseudo database of messages
-var messages []Message
+var messages []models.ChatMessage
 
 func init() {
 	apis.RegisterHandler(fiber.MethodGet, "/chat", auth.Public, getMessages)
-	apis.RegisterHandler(fiber.MethodPost, "/chat", auth.Public, postMessage)
+	apis.RegisterHandler(fiber.MethodPost, "/chat", auth.Public, sendMessage)
 }
 
-// Return all messages belonging to a certain recipient
-func getMessages(context *fiber.Ctx) error {
-	name := context.Query("name")
-	var chatHistory []Message
-
-	for _, msg := range messages {
-		if msg.To == name || msg.From == name {
-			chatHistory = append(chatHistory, msg)
-		}
-	}
-
-	return context.JSON(chatHistory)
-}
-
-// Simple post message function which adds to our messages array.
-func postMessage(context *fiber.Ctx) error {
-	var message Message
-
+func sendMessage(context *fiber.Ctx) error {
+	var message models.ChatMessage
 	err := context.BodyParser(&message)
+
 	if err != nil {
-		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error":    "Cannot parse JSON",
-			"received": message.Content,
-		})
-	}
-
-	isMessageValid := message.Content != "" &&
-		message.From != "" &&
-		message.To != ""
-
-	if !isMessageValid {
-		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Bad info.  'To', 'From', and 'Content' must not be empty...",
-		})
+		return err
 	}
 
 	messages = append(messages, message)
 
-	return context.SendStatus(fiber.StatusCreated)
+	return context.JSON(fiber.Map{
+		"status": "success",
+		"sent":   "true",
+		"data":   message,
+	})
+}
+
+func getMessages(context *fiber.Ctx) error {
+	userID := context.Params("userID")
+	var missedMessages []models.ChatMessage
+
+	for _, message := range messages {
+		if message.RecipientID == userID {
+			missedMessages = append(missedMessages, message)
+		}
+	}
+
+	return context.JSON(missedMessages)
 }
